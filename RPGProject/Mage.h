0d1f842faff4758089b79511ca4f059ca116dda5
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-05-11 13:35:13 -0700

    Add project files.

---
+++ RPGProject/Mage.h
@@ -0,0 +1,19 @@
+#ifndef MAGE_H
+#define MAGE_H
+#include "Player.h"
+
+class Mage : public Player
+{
+public:
+	Mage(string name, Race race) : Player(name, race, 150, 150)
+	{
+
+	}
+
+	string attack() const
+	{
+		return "I will crush you with my Arcane Missles!";
+	}
+};
+
+#endif
---
+++ RPGProject/Player.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+using namespace std;
+
+Player::Player(string name, Race race, int hitPoints, int magicPoints)
+{
+	this->name = name;
+	this->race = race;
+	this->hitPoints = hitPoints;
+	this->magicPoints = magicPoints;
+}
+
+// ------------------------- getters
+string Player::getName() const
+{
+	return name;
+}
+
+Race Player::getRace() const
+{
+	return race;
+}
+
+
+string Player::whatRace() const
+{
+	string result = "";
+
+	if (race == HUMAN)
+	{
+		result = "Human";
+	}
+	else if (race == ELF)
+	{
+		result = "Elf";
+	}
+	else if (race == DWARF)
+	{
+		result = "Dwarf";
+	}
+	else if (race == ORC)
+	{
+		result = "Orc";
+	}
+	else if (race == TROLL)
+	{
+		result = "Troll";
+	}
+
+	return result;
+}
+
+int Player::getHitPoints() const
+{
+	return hitPoints;
+}
+
+int Player::getMagicPoints() const
+{
+	return magicPoints;
+}
+
+// ------------------------------- setters
+void Player::setName(string name)
+{
+
+}
+
+void Player::setRace(Race race)
+{
+	this->race = race;
+}
+
+void Player::setHitPoints(int hitpoints)
+{
+	this->hitPoints = hitPoints;
+}
+
+void Player::setMagicPoints(int magicPoints)
+{
+	this->magicPoints = magicPoints;
+}
